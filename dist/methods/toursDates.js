7fb269e17912e179dfb5af5521bf66e2728ae192
Vladyslav Podolskyi <podolskyi.vladyslav@airslate.com>
2023-11-29 20:43:56 +0100

    updated get tours dates method

--- dist/methods/toursDates.js
+++ dist/methods/toursDates.js
@@ -5,9 +5,23 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports.getToursDates = getToursDates;
 
+var _ramda = require("ramda");
+
 var _fn = require("../fn");
 
 var _config = require("../config");
+
+function _slicedToArray(arr, i) { return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _unsupportedIterableToArray(arr, i) || _nonIterableRest(); }
+
+function _nonIterableRest() { throw new TypeError("Invalid attempt to destructure non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); }
+
+function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
+
+function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
+
+function _iterableToArrayLimit(arr, i) { if (typeof Symbol === "undefined" || !(Symbol.iterator in Object(arr))) return; var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"] != null) _i["return"](); } finally { if (_d) throw _e; } } return _arr; }
+
+function _arrayWithHoles(arr) { if (Array.isArray(arr)) return arr; }
 
 function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
 
@@ -42,7 +56,22 @@ function _getToursDates() {
             _yield$makeCall = _context.sent;
             _yield$makeCall$dates = _yield$makeCall.dates;
             denormalizedDates = _yield$makeCall$dates === void 0 ? {} : _yield$makeCall$dates;
-            return _context.abrupt("return", Object.keys(denormalizedDates));
+            return _context.abrupt("return", (0, _ramda.map)(function (_ref) {
+              var _ref2 = _slicedToArray(_ref, 2),
+                  date = _ref2[0],
+                  rangeAsString = _ref2[1];
+
+              var rangeAsSortedArray = (0, _ramda.call)((0, _ramda.pipe)((0, _ramda.split)(','), (0, _ramda.map)(Number), (0, _ramda.sort)((0, _ramda.ascend)(function (range) {
+                return range;
+              }))), rangeAsString);
+              return {
+                date: date,
+                range: {
+                  from: (0, _ramda.head)(rangeAsSortedArray),
+                  to: (0, _ramda.last)(rangeAsSortedArray)
+                }
+              };
+            }, (0, _ramda.toPairs)(denormalizedDates)));
 
           case 6:
           case "end":
--- src/methods/toursDates.js
+++ src/methods/toursDates.js
@@ -1,14 +1,37 @@
-// Instruments
+import { ascend, call, head, last, map, pipe, sort, split, toPairs } from 'ramda';
+
 import { makeCall } from '../fn';
 import { ENDPOINTS } from '../config';
 
 export async function getToursDates (token, options) {
-    const { dates : denormalizedDates = {}} = await makeCall({ endpoint: ENDPOINTS.dates,
+    const { dates : denormalizedDates = {}} = await makeCall({
+        endpoint: ENDPOINTS.dates,
         query:    {
             ...token,
             ...options,
         },
-        ttl: [2, 'hour']});
+        ttl: [2, 'hour'],
+    });
 
-    return Object.keys(denormalizedDates);
+    return map(
+        ([date, rangeAsString]) => {
+            const rangeAsSortedArray = call(
+                pipe(
+                    split(','),
+                    map(Number),
+                    sort(ascend((range) => range))
+                ),
+                rangeAsString
+            );
+
+            return {
+                date,
+                range: {
+                    from: head(rangeAsSortedArray),
+                    to:   last(rangeAsSortedArray),
+                },
+            };
+        },
+        toPairs(denormalizedDates)
+    );
 }
