d29e6c6087001ec1ebe81e49a1df9928759abe7c
Anatolii Kosolapov <trogwar4ik4@gmail.com>
2025-10-09 16:20:56 +0300

    transiled for task-47739

--- dist/static.js
+++ dist/static.js
@@ -392,7 +392,6 @@ var TOUR_OPTIONS = exports.TOUR_OPTIONS = {
   NOT_NEED_VISA: 'notNeedVisa',
   NEVER_LAND: 'neverland',
   LUGGAGE: 'luggage',
-  LUGGAGE_BY_REQUEST: 'luggagerequest',
   EXCURSION: 'excursion',
   PRESTIGE: 'prestige',
   ELSE: 'else',
